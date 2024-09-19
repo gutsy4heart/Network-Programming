e5446a1c043754f632cf338b97b5e0a9e1a9ac7f
Murad <murad.nurmammadli@gmail.com>
2024-09-20 00:15:24 +0400

    N

---
+++ SocketPractice/Client/TimeClient.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+public class TimeClient
+{
+    public static void Main(string[] args)
+    {
+        TCMethod();
+    }
+
+    public static void TCMethod()
+    {
+        try
+        {
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+            clientSocket.Connect(serverEndPoint);
+
+            Console.WriteLine("Введите запрос (time или date): ");
+            string request = Console.ReadLine();
+
+            byte[] requestBytes = Encoding.UTF8.GetBytes(request);
+            clientSocket.Send(requestBytes);
+
+            byte[] buffer = new byte[1024];
+            int receivedBytes = clientSocket.Receive(buffer);
+            string response = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+
+            Console.WriteLine($"Ответ от сервера: {response}");
+
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ SocketPractice/Server/TimeServer.cs
@@ -0,0 +1,60 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Linq.Expressions;
+
+public class TimeServer
+{
+    public static void Main(string[] args)
+    {
+        TSMethod();
+    }
+    public static void TSMethod()
+    {
+        try
+        {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+            server.Bind(ep);
+
+            server.Listen(10);
+            Console.WriteLine("Сервер запущен. Ожидание подключения клиентов...");
+
+            while (true)
+            {
+                Socket clientSocket = server.Accept();
+                Console.WriteLine("Клиент подключен.");
+
+                byte[] buffer = new byte[1024];
+                int receivedBytes = clientSocket.Receive(buffer);
+                string request = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+
+                string response = "";
+                if (request.ToLower() == "time")
+                {
+                    response = DateTime.Now.ToString("HH:mm:ss");
+                }
+                else if (request.ToLower() == "date")
+                {
+                    response = DateTime.Now.ToString("yyyy-MM-dd");
+                }
+                else
+                {
+                    response = "Invalid request";
+                }
+
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                clientSocket.Send(responseBytes);
+                Console.WriteLine($"Отправлен ответ клиенту: {response}");
+
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+}
