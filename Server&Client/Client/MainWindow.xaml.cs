46cf2471054a7e3b53cd89b5343578908616e42b
Murad <murad.nurmammadli@gmail.com>
2024-09-21 23:48:03 +0400

    Socket

---
+++ Server&Client/Client/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Socket clientSocket;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void SendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (clientSocket == null || !clientSocket.Connected)
+                {
+                    clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    IPAddress ip = IPAddress.Parse("127.0.0.1");
+                    IPEndPoint iep = new IPEndPoint(ip, 8080);
+                    clientSocket.Connect(iep);
+                }
+
+                string message = MessageInput.Text;
+                byte[] buffer = Encoding.ASCII.GetBytes(message);
+                clientSocket.Send(buffer);
+
+                MessagesList.Items.Add(DateTime.Now.ToString() + "  Sent: " + message);
+                MessageInput.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessagesList.Items.Add("Client Error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Server&Client/Server/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Server
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Socket socket;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void StartServer_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                try
+                {
+                    socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    IPAddress ip = IPAddress.Parse("127.0.0.1");
+                    IPEndPoint iep = new IPEndPoint(ip, 8080);
+                    socket.Bind(iep);
+                    socket.Listen(10);
+
+                    Dispatcher.Invoke(() => MessagesList.Items.Add(DateTime.Now.ToString() + "  Waiting for connections..."));
+
+                    while (true)
+                    {
+                        var clientSocket = socket.Accept();
+                        Task.Run(() =>
+                        {
+                            while (true)
+                            {
+                                byte[] buffer = new byte[1024];
+                                int dataSize = clientSocket.Receive(buffer);
+                                string data = Encoding.ASCII.GetString(buffer, 0, dataSize);
+
+                                Dispatcher.Invoke(() => MessagesList.Items.Add(DateTime.Now.ToString() + "  " + data));
+                            }
+                        });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Dispatcher.Invoke(() => MessagesList.Items.Add("Server Error: " + ex.Message));
+                }
+            });
+        }
+    }
+}
