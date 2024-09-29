f5bac44a108178ae33739af53b9fc6ebb5c16193
Murad <murad.nurmammadli@gmail.com>
2024-09-30 00:19:44 +0400

    Rock-Paper-Scissors-Lizard-Spock

---
+++ Rock-Paper-Scissors-Lizard-Spock/Rock-Paper-Scissors-Lizard-Spock/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Rock_Paper_Scissors_Lizard_Spock
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
